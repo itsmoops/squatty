3dca8f0e8c5285ec6599da1b6751d9608d14fe64
Moore, Eric-CW <Eric.Moore2@bestbuy.com>
2017-03-03 15:02:21 -0600

    check in email work

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const request = require("request")
 const moment = require('moment')
 const whois = require('whois-api')
 const jsonfile = require('jsonfile')
+const nodemailer = require('nodemailer');
 
 const googleDomains = []
 
@@ -34,7 +35,6 @@ const getTrendingGoogleSearchDomains = () => {
     if (body) {
       let data = JSON.parse(body)
       if (data) {
-        googleDomains.push(`blarhghhhhhggggg.com`)
         data[1].forEach((searchTerm) => {
           let sanitizedDomain = sanitizeDomains(searchTerm)
           googleDomains.push(sanitizedDomain)
@@ -112,18 +112,83 @@ app.get('/domains', (request, response) => {
       if (today.isAfter(domains[0].DATE_GENERATED, 'hour')) {
         // if it's been more than a day, regenerate domains
         getDomains(filePath).then(domainInfo => {
+          sendEmail()
           response.json(domainInfo)
         })
       } else {
         // if it hasn't been a day, don't regenerate
+        sendEmail()
         response.json(domains)
       }
     } else {
       // if it doesn't exist, create a file
       getDomains(filePath).then(domainInfo => {
         // write json file
+        sendEmail()
         response.json(domainInfo)
       })
     }
   })
 })
+
+
+
+// EMAIL SETTINGS //
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'squattydomains@gmail.com',
+        pass: 'SquattyPotty87'
+    }
+});
+
+const sendEmail = () => {
+  const domains = require(`./data/domains.json`)
+  let linkContainerStyle = `width:100%;padding-left:5px;`
+  let availableStyle = `color:#55c16a;`
+  let unavailableStyle = `color:#c94646;`
+  let domainLinks = ``
+  domains.forEach((domain, idx) => {
+    if (idx > 0) {
+      if (domain.available) {
+        domainLinks += `<div style="${linkContainerStyle}">
+                          <a style="${availableStyle}" href=https://www.namecheap.com/domains/registration/results.aspx?domain=${domain}>${domain.URL}</a>
+                        </div>`
+      } else {
+        domainLinks += `<div style="${linkContainerStyle}">
+                          <a style="${unavailableStyle}" href=https://www.namecheap.com/domains/registration/results.aspx?domain=${domain}>${domain.URL}</a>
+                        </div>`
+      }
+    }
+  })
+  // setup email data with unicode symbols
+  let containerStyle = `width:100%;height:100%;background-color:#222;color:white;font-family:Arial;`
+  let centeredTextStyle = `text-align:center;`
+  let mailBody = `<html>
+                    <div style="${containerStyle}">
+                      <h1 style="${centeredTextStyle} padding-top:10px;">
+                        Squatty Domains
+                      </h1>
+                      <h2 style="${centeredTextStyle} font-size: 20px;">
+                        ${moment().format('MMMM Do YYYY, h:mm a')}
+                      </h2>
+                      <div>
+                        ${domainLinks}
+                      </div>
+                    </div>
+                  </html>`
+  let mailOptions = {
+      from: `"Squatty Domains 💩" <squattydomains@gmail.com>`, // sender address
+      to: `moore.ericc@gmail.com, karl.schwende@gmail.com`, // list of receivers, comma separate to add more
+      subject: `Available Domains`, // Subject line
+      html: mailBody // html body
+  }
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
