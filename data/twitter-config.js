12d44c559fb01594a2bcfb0eee7a1807df60b48f
Eric Moore <moore.ericc@gmail.com>
2017-03-06 22:52:44 -0600

    twitter config

---
+++ data/twitter-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "consumerKey": "OtodVmopRz5y1EN66wFEzwYD0",
+  "consumerSecret": "EUULw49ZtvPDp8YGfNN5vwuQQPfOyLSzF7AlB1kJJWhmy0AWRB",
+  "accessToken": "862420878-f1lJSxLwjLMHGx81HLG3jSjNqnJHvQFDSAZla6F5",
+  "accessTokenSecret": "UJ6EhBLDPjJmrc9D8hgCJOgzEmRGl8d2WZYC1VoaOtbcV",
+  "callBackUrl": "http://www.localhost:8080/"
+}
